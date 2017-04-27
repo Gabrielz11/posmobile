24e7adbad234981130ae36ff07dbc86414c812e4
Djonathas Cardoso <djonathas.cardoso@crptecnologia.com.br>
2017-04-27 18:05:14 -0300

    adicionando funcionalidade de acelerometro

---
+++ trabalho-djonathas/trabalho_final/src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {DeviceMotion, DeviceMotionAccelerationData} from "@ionic-native/device-motion";
+import { LoadingController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public dados:DeviceMotionAccelerationData;
+  public loading:boolean = true;
+
+  constructor(public navCtrl: NavController, public deviceMotion: DeviceMotion, public loadingCtrl: LoadingController) {
+    // Watch device acceleration
+    let subscription = this.deviceMotion.watchAcceleration().subscribe((acceleration: DeviceMotionAccelerationData) => {
+      this.dados = acceleration;
+    });
+
+    let loader = this.loadingCtrl.create({
+      content: "Obtendo dados do acelerômetro...",
+      duration: 10000
+    });
+
+    loader.present();
+
+    setTimeout(() => {
+      this.loading = false;
+      loader.dismiss();
+    }, 10000);
+  }
+
+
+}
